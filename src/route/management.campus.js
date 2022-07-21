0c7c131539ad8db1ab7633ddca40358749f5ab86
14Kgun <geon6757@kaist.ac.kr>
2022-07-22 05:23:20 +0900

    Update /route/management.campus.js

--- src/route/management.campus.js
+++ src/route/management.campus.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { teacherModel } = require('../db/mongo');
 const { query, param, body, validationResult } = require("express-validator");
 const patterns = require('../db/regExpPatterns');
+const bkfd2Password = require("pbkdf2-password");
+const hasher = bkfd2Password();
 const router = express.Router();
 
 router.use(require('../middlewares/authTeacherCampus'));
@@ -119,6 +121,67 @@ router.post("/teacher/edit/name", [
     console.log(e);
     return res.status(401).json({
       error: "/management/campus/teacher/edit/name : internal server error"
+    })
+  }
+})
+
+router.post("/teacher/edit/password", [
+  body("id").matches(patterns.loginId),
+  body("password").matches(patterns.loginPw),
+], async (req, res) => {
+  const validationErrors = validationResult(req);
+  if (!validationErrors.isEmpty()) {
+    return res.status(400).json({
+      error: "management/campus/teacher/edit/password : bad request",
+    });
+  }
+
+  try {
+    const teacher = await teacherModel.findOne({
+      id: req.body.id
+    });
+    if (!teacher) {
+      return res.status(403).json({
+        error: "management/campus/edit/password : no corresponding teacher"
+      })
+    }
+    if (teacher.campus.toString() != req.campusId.toString()) {
+      return res.status(402).json({
+        error: "management/campus/teacher/edit/password : Permission denied"
+      });
+    }
+    if (req.loginLevel != 'administrator' && req.loginLevel == 'director') {
+      if (req.body.id != req.loginId) {
+        return res.status(402).json({
+          error: "management/campus/teacher/edit/password : Permission denied"
+        });
+      }
+    }
+
+    hasher({ password: req.body.password }, async (err, pass, salt, hash) => {
+      if (err) {
+        console.log(err);
+        return res.status(401).json({
+          error: "/management/campus/teacher/edit/password : internal server error"
+        })
+      }
+      const teacherAfter = await teacherModel.findOneAndUpdate({
+        id: req.body.id
+      }, {
+        salt: salt,
+        password: hash,
+      }, {
+        new: true
+      });
+      res.json({
+        teacher: teacherAfter
+      });
+    })
+  }
+  catch (e) {
+    console.log(e);
+    return res.status(401).json({
+      error: "/management/campus/teacher/edit/password : internal server error"
     })
   }
 })
